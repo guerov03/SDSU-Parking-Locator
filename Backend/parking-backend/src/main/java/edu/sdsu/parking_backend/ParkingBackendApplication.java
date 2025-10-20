6bf01b4db80d4c87c23eaf96ed93efe2bfdcb315
usahanA-lexus <aaguirrearias4977@sdsu.edu>
2025-10-19 21:16:30 -0700

    Initial project setup and configuration

---
+++ Backend/parking-backend/src/main/java/edu/sdsu/parking_backend/ParkingBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.sdsu.parking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/parking-backend/src/test/java/edu/sdsu/parking_backend/ParkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sdsu.parking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
