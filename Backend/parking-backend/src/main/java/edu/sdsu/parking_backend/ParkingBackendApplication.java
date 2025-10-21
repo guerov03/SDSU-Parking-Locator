7e3e9d6d47beee6c2a4cedd4cc8ffc0996338e29
usahanA-lexus <aaguirrearias4977@sdsu.edu>
2025-10-21 14:38:34 -0700

    refactor: update main app config

--- Backend/parking-backend/src/main/java/edu/sdsu/parking_backend/ParkingBackendApplication.java
+++ Backend/parking-backend/src/main/java/edu/sdsu/parking_backend/ParkingBackendApplication.java
@@ -2,12 +2,18 @@ package edu.sdsu.parking_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-public class ParkingBackendApplication {
+public class ParkingBackendApplication 
+{
+	private static final Logger log = LoggerFactory.getLogger(ParkingBackendApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkingBackendApplication.class, args);
-	}
+    public static void main(String[] args) 
+	{
+        SpringApplication.run(ParkingBackendApplication.class, args);
+        log.info("Parking Backend started successfully.");
+    }
 
 }
